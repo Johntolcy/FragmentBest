7627a2a6a01e056e25403239625da17815c800a3
Johnny <18201104668@163.com>
2017-08-29 20:43:21 +0800

    简单新闻界面

---
+++ app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.fragmentbestpractice;
+
+/**
+ * Created by Administrator on 2017/8/29.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
